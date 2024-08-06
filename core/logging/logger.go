0f6b1140e76f3f33065c7a7e53b38886026b77b1
Ian Castaño <ianfe@martina.com.co>
2024-08-06 17:19:42 -0500

    feat: Added loggers from uber zap

---
+++ core/logging/logger.go
@@ -0,0 +1,42 @@
+package logging
+
+import (
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"os"
+)
+
+// LoggerConfig contains the logger setup configuration.
+type LoggerConfig struct {
+	LogFile   *os.File
+	UseColors bool
+}
+
+// NewLogger configures a new logger based on LoggerConfig data.
+func NewLogger(config LoggerConfig) (*zap.Logger, error) {
+
+	consoleEncoderConfig := zap.NewDevelopmentEncoderConfig()
+	consoleEncoderConfig.EncodeLevel = zapcore.CapitalLevelEncoder
+	if config.UseColors {
+		consoleEncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
+	}
+	consoleEncoder := zapcore.NewConsoleEncoder(consoleEncoderConfig)
+
+	jsonEncoderConfig := zap.NewProductionEncoderConfig()
+	jsonEncoder := zapcore.NewJSONEncoder(jsonEncoderConfig)
+
+	// Create the core based on the presence of a log file
+	var cores []zapcore.Core
+	cores = append(cores, zapcore.NewCore(consoleEncoder, zapcore.AddSync(os.Stdout), zapcore.DebugLevel))
+	if config.LogFile != nil {
+		fileCore := zapcore.NewCore(jsonEncoder, zapcore.AddSync(config.LogFile), zapcore.DebugLevel)
+		cores = append(cores, fileCore)
+	}
+
+	// Create the logger
+	core := zapcore.NewTee(cores...)
+	logger := zap.New(core, zap.AddCaller())
+
+	return logger, nil
+
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module discord-sentinel
 
 go 1.22
+
+require go.uber.org/zap v1.27.0
+
+require go.uber.org/multierr v1.11.0 // indirect
